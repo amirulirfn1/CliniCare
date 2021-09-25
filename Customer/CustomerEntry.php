ae165e6ea48b68d8e7ff554e66191abd6fbd6ead
AMIRUL IRFAN BIN AHMAD SOPHIAN <88524446+amirulirfn1@users.noreply.github.com>
2021-09-26 00:41:49 +0800

    Update email template, requirement for signup signin details

--- Customer/CustomerEntry.php
+++ Customer/CustomerEntry.php
@@ -57,10 +57,141 @@ function signup()
         $headers .= "MIME-Version : 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-        $message = "<a href='http://localhost/MasterCliniCare/Customer/Verify.php?vkey=$vkey'>
-		Click Here To Verify Your Email Address</a>";
+        $htmlContent = '<!doctype html>
+        <html>
+            <head>
+                <meta charset=utf-8>
+                <meta name=viewport content=width=device-width, initial-scale=1>
+                <title>Snippet - GoSNippets</title>
+                <link href=https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css rel=stylesheet>
+                <link  rel=stylesheet>
+                <style> 
 
-        mail($to, $subject, $message, $headers);
+/* CLIENT-SPECIFIC STYLES */
+body,
+table,
+td,
+a {
+-webkit-text-size-adjust: 100%;
+-ms-text-size-adjust: 100%;
+}
+
+table,
+td {
+mso-table-lspace: 0pt;
+mso-table-rspace: 0pt;
+}
+
+img {
+-ms-interpolation-mode: bicubic;
+}
+
+/* RESET STYLES */
+img {
+border: 0;
+height: auto;
+line-height: 100%;
+outline: none;
+text-decoration: none;
+}
+
+table {
+border-collapse: collapse !important;
+}
+
+body {
+height: 100% !important;
+margin: 0 !important;
+padding: 0 !important;
+width: 100% !important;
+background-color: #f4f4f4;
+}
+
+/* iOS BLUE LINKS */
+a[x-apple-data-detectors] {
+color: inherit !important;
+text-decoration: none !important;
+font-size: inherit !important;
+font-family: inherit !important;
+font-weight: inherit !important;
+line-height: inherit !important;
+}
+
+/* MOBILE STYLES */
+@media screen and (max-width:600px) {
+h1 {
+font-size: 32px !important;
+line-height: 32px !important;
+}
+}
+
+/* ANDROID CENTER FIX */
+div[style*="margin: 16px 0;"] {
+margin: 0 !important;
+}</style>
+                <script type=text/javascript ></script>
+                <script type=text/javascript src=https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js></script>
+                <script type=text/javascript src=https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js></script>
+            </head>
+            <body oncontextmenu=return false class=snippet-body>
+            <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: Lato, Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> Were thrilled to have you here! Get ready to dive into your new account. </div>
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<!-- LOGO -->
+<tr>
+<td bgcolor="#ffffff" align="center">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+</table>
+</td>
+</tr>
+<tr>
+<td bgcolor="#ffffff" align="center" style="padding: 0px 10px 0px 10px;">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+<tr>
+    <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+        <h1 style="font-size: 42px; font-weight: 400; margin: 2;">Welcome, ' . $name .' !</h1> <img src="https://i.imgur.com/KGGAQuG.png" width="125" height="120" style="display: block; border: 0px;" />
+    </td>
+</tr>
+</table>
+</td>
+</tr>
+<tr>
+<td bgcolor="#ffffff" align="center" style="padding: 0px 10px 0px 10px;">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+<tr>
+    <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+        <p style="margin: 0;">Were excited to have you get started. First, you need to confirm your account. Just press the button below.</p>
+    </td>
+</tr>
+<tr>
+    <td bgcolor="#ffffff" align="left">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
+                    <table border="0" cellspacing="0" cellpadding="0">
+                        <tr>
+                            <td align="center" style="border-radius: 3px;" bgcolor="#e0edfb"><a href="http://localhost/MasterCliniCare/Customer/Verify.php?vkey=' .$vkey .'" target="_blank" 
+                            style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #000000; text-decoration: none; color: #000000; 
+                            text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #e0edfb; display: inline-block;">
+                            Confirm Account</a></td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr> 
+
+</table>
+</td>
+</tr>
+</table>
+                <script type=text/javascript></script>
+            </body>
+        </html>';
+        //$message = "<a href='http://localhost/MasterCliniCare/Customer/Verify.php?vkey=$vkey'>
+		//Click Here To Verify Your Email Address</a>";
+
+        mail($to, $subject, $htmlContent, $headers);
 
         $sql2 = "INSERT INTO user (email, password, usertype)
                             VALUES('$email', '$password', 'customer')";
